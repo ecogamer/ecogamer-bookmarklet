67e42b77935522ae44fc8f576d1194c941de2e64
Anthony Felton <anthony@hunterwebs.com.au>
2016-02-09 12:07:12 +1100

    no idea. uncommitted stuff from old computer

--- bookmarklet/initilise.js
+++ bookmarklet/initilise.js
@@ -1,29 +1,15 @@
+// TODO move this file to /js folder
 (function(){
 
     // Initilisation script should only run once
-    alert('test');
 
-    // Load jQuery
-    // TODO: if jquery is present, move it to something, load my jquery, put my jquery into noconflict and put original jquery back. ALTERNATIVELY use requirejs grunt plugin?
-    var done = false;
-    var jQueryVersion = '1.9.1';
-    var jQueryScriptTag = document.createElement('script');
-    jQueryScriptTag.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jQueryVersion + '/jquery.min.js';
-    jQueryScriptTag.onload = jQueryScriptTag.onreadystatechange = function(){
-        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-            done = true;
-        }
-    };
-    document.getElementsByTagName('head')[0].appendChild(jQueryScriptTag);
-    jQuery.noConflict();
+    // requirejs load all scripts here (or above, outside function, wherever its supposed to go)
 
-    // Load bookmarklet scripts
-    jQuery(document).ready(function($) {
-        // include js and CSS
-    });
+    // load css here, any additional items as well (scripts? not sure)
 
     // Run bookmarklet
     var ecogamerBookmarklet = function() {
+        // execute popup
         alert('test');
     };
 
